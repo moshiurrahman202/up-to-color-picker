46516a84f98e9ac15d0c31991e4a38a2bd9b5fe6
Mosiur Rahman <mosiurrahman123654@gmail.com>
2022-05-15 18:48:26 +0600

    Done Hex color generator

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /*
 => Requirements
-=> Change The Background Color By Randon RGB Color By Clocking a Button
+=> Change The Background Color By Randon Hex Color By Clocking a Button
+=> Also dispaly the hex code to a disabled input field
 */
 
 
@@ -10,12 +11,24 @@ window.onload = () =>{
 }
 
 function main(){
-
+ const root = document.getElementById("root");
+ const btn = document.getElementById("btn");
+ const output = document.getElementById("output");
+ btn.addEventListener("click", () =>{
+     const bgColor = genHexcol();
+     root.style.backgroundColor = bgColor;
+     output.value = bgColor;
+ });
 }
 // Step 2 => random rgb color generator function
-function genRGBcol(){
+function genHexcol(){
     // formate of rgb color high=> rgb(0,0,0) low=> rgb(255,255,255)
-    
+    // 255, 255, 255 =hex-> #ffffff
+    const red = Math.floor(Math.random() *255);
+    const green = Math.floor(Math.random() *255);
+    const blue = Math.floor(Math.random() *255);
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+
 }
 // Step 3 => collect all necerssry referrences
 // Step 4 => handel the click event
