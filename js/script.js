2b4b81869b8e714d7e6efd3a61057a112f73ea90
Mosiur Rahman <mosiurrahman123654@gmail.com>
2022-05-15 18:07:48 +0600

    Done rgb generator or background changer

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*
+=> Requirements
+=> Change The Background Color By Randon RGB Color By Clocking a Button
+*/
+
+
+// Step 1 => create onload handler
+window.onload = () =>{
+    main();
+}
+
+function main(){
+
+}
+// Step 2 => random rgb color generator function
+function genRGBcol(){
+    // formate of rgb color high=> rgb(0,0,0) low=> rgb(255,255,255)
+    
+}
+// Step 3 => collect all necerssry referrences
+// Step 4 => handel the click event
