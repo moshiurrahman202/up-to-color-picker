19fc4d9a0a3236867c307df4fe1925dfbd5b419e
Mosiur Rahman <mosiurrahman123654@gmail.com>
2022-05-17 01:57:51 +0600

    Add a dynamic toast message when copyed

--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@
 => Requirements
 => Change The Background Color By Randon Hex Color By Clocking a Button
 => Also dispaly the hex code to a disabled input field
-=> Add a button to copy the hex code;
+=> Add a button to copy the hex code
+=> Add a toast message when copyed
 */
 
+// Globals
+let div = null;
 
 // Step 1 => create onload handler
 window.onload = () =>{
@@ -25,6 +28,11 @@ function main(){
 
  copybtn.addEventListener("click", () =>{
      navigator.clipboard.writeText(output.value);
+     if(div !== null){
+         div.remove();
+         div = null;
+     }
+     geberateToastMessage(`${output.value} copied`)
  });
 }
 // Step 2 => random rgb color generator function
@@ -37,6 +45,26 @@ function genHexcol(){
     return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 
 }
+
+function geberateToastMessage(msg){
+   div = document.createElement("div");
+    div.innerText = msg;
+    div.className = "toast-message toast-message-slide-in";
+    div.addEventListener("click", ()=>{
+        div.classList.remove("toast-message-slide-in");
+        div.classList.add("toast-message-slide-out");
+
+        div.addEventListener("animationend", ()=>{
+            div.remove();
+            div = null;
+        })
+    })
+    document.body.appendChild(div);
+
+}
 // Step 3 => collect all necerssry referrences
 // Step 4 => handel the Change Button click event
 // Step 5 => handle the copy Button Click event
+// Step 6 => activate toast message 
+// Step 7 => Create a dynamic toast message
+// Step 8 => Cleare toast message 
