8424891f65ad0ac88b44fa3a319a8e2e503318e3
Mosiur Rahman <mosiurrahman123654@gmail.com>
2022-05-17 17:07:43 +0600

    User can type ther own hex code too

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@
 => Change The Background Color By Randon Hex Color By Clocking a Button
 => Also dispaly the hex code to a disabled input field
 => Add a button to copy the hex code
-=> Add a toast message when copyed
+=> Add a toast message when copied
+=> User can type ther own hex code too
 */
 
 // Globals
@@ -32,9 +33,20 @@ function main(){
          div.remove();
          div = null;
      }
-     geberateToastMessage(`${output.value} copied`)
+     if(isHexValid(output.value)){
+        geberateToastMessage(`${output.value} copied`)
+     }else{
+         alert("Invalid color code!")
+     }
  });
 }
+
+output.addEventListener("keyup", function(e){
+    const color = e.target.value;
+    if(color && isHexValid(color)){
+        root.style.backgroundColor = color;
+    }
+});
 // Step 2 => random rgb color generator function
 function genHexcol(){
     // formate of rgb color high=> rgb(0,0,0) low=> rgb(255,255,255)
@@ -62,9 +74,23 @@ function geberateToastMessage(msg){
     document.body.appendChild(div);
 
 }
+/**
+ * 
+ * @param {string} color 
+ */
+function isHexValid(color){
+    if(color.length !== 7)return false;
+    if (color[0] !== "#") return false;
+
+    color = color.substring(1);
+    return/^[0-9A-Fa-f]{6}$/i.test(color);
+}
 // Step 3 => collect all necerssry referrences
 // Step 4 => handel the Change Button click event
 // Step 5 => handle the copy Button Click event
 // Step 6 => activate toast message 
 // Step 7 => Create a dynamic toast message
 // Step 8 => Cleare toast message 
+// Step 9 => Create isHexValid function
+// Step 10 => Implement change handler on input field
+// Step 11 => prevent copy Hex code if it is not valid
