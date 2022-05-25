1dd1dcdceea671b6b02f303144097ba06725ab29
Mosiur Rahman <mosiurrahman123654@gmail.com>
2022-05-25 18:40:20 +0600

    input mode color and input slaider done

--- js/script.js
+++ js/script.js
@@ -12,23 +12,24 @@ window.onload = () =>{
 
 // main or boot function
 function main(){
-    const generaterandomcolorbtn = document.getElementById("generate-random-color")
-//  const root = document.getElementById("root");
-//  const changebtn = document.getElementById("changebtn");
-//  const output = document.getElementById("output");
-//  const output2 = document.getElementById("output2");
-//  const copybtn = document.getElementById("copy-btn");
-//  const copybtn2 = document.getElementById("copy-btn2");
-
- // for change color
+    // dom references
+    const generaterandomcolorbtn = document.getElementById("generate-random-color");
+    const colorModeHexInp = document.getElementById("input-hex");
+    const colorSlaiderRed = document.getElementById("color-slider-red");
+    const colorSlaiderGreen = document.getElementById("color-slider-green");
+    const colorSlaiderBlue = document.getElementById("color-slider-blue");
+ // // event listeners
  generaterandomcolorbtn.addEventListener("click", generaterandomcolorforbtn);
+ colorModeHexInp.addEventListener("keyup", colorModeHexForInp);
+ colorSlaiderRed.addEventListener("change", handleColerSlider(colorSlaiderRed, colorSlaiderGreen, colorSlaiderBlue));
+ colorSlaiderGreen.addEventListener("change", handleColerSlider(colorSlaiderRed, colorSlaiderGreen, colorSlaiderBlue));
+ colorSlaiderBlue.addEventListener("change", handleColerSlider(colorSlaiderRed, colorSlaiderGreen, colorSlaiderBlue));
+//  // event handlears
+//  function generaterandomcolorforbtn () {
+//     const color = generateColorDecimal();
+//    updateColorCodeToDom(color)
+// }
 
- 
- // event handlears
- function generaterandomcolorforbtn () {
-    const color = generateColorDecimal();
-   updateColorCodeToDom(color)
-}
  // dom functions
  // for toast message
  function geberateToastMessage(msg){
@@ -48,26 +49,9 @@ function main(){
  
  }
 
- /**
-  * update dom element with calculated color values
-  * @param {object}  color
-  */
- function updateColorCodeToDom(color){
-    const hexcolor = genHexcol(color);
-    const rgbcolor = genRgbCol(color);
+ 
 
-    document.getElementById("color-display").style.backgroundColor = hexcolor;
-    document.getElementById("input-hex").value = hexcolor;
-    document.getElementById("input-rgb").value =rgbcolor;
-    document.getElementById("color-slider-red").value = color.red;
-    document.getElementById("color-slider-red-label").innerText = color.red;
-    document.getElementById("color-slider-green").value = color.green;
-    document.getElementById("color-slider-green-label").innerText = color.green;
-    document.getElementById("color-slider-blue").value = color.blue;
-    document.getElementById("color-slider-blue-label").innerText = color.blue;
- }
 
- //  utils
  // for hex copy 
 //  copybtn.addEventListener("click", () =>{
 //      navigator.clipboard.writeText(`#${output.value}`);
@@ -96,6 +80,55 @@ function main(){
 //     }
 // });
 }
+
+ // event handlears
+ function generaterandomcolorforbtn () {
+    const color = generateColorDecimal();
+   updateColorCodeToDom(color)
+ }
+
+ function colorModeHexForInp(e){
+    const hexcolor = e.target.value;
+    if(hexcolor){
+        this.value = hexcolor.toUpperCase();
+        if(isHexValid(hexcolor)){
+            const color = hexToDecimalColors(hexcolor)
+            updateColorCodeToDom(color);
+        }
+    }
+}
+
+function handleColerSlider(colorSlaiderRed, colorSlaiderGreen, colorSlaiderBlue){
+    return function (){
+        const color = {
+            red: parseInt(colorSlaiderRed.value),
+            green: parseInt(colorSlaiderGreen.value),
+            blue: parseInt(colorSlaiderBlue.value),
+        };
+        updateColorCodeToDom(color);
+    };
+}
+
+  //  utils
+  /**
+  * update dom element with calculated color values
+  * @param {object}  color
+  */
+ function updateColorCodeToDom(color){
+    const hexcolor = genHexcol(color);
+    const rgbcolor = genRgbCol(color);
+
+    document.getElementById("color-display").style.backgroundColor = `#${hexcolor}`;
+    document.getElementById("input-hex").value = hexcolor;
+    document.getElementById("input-rgb").value =rgbcolor;
+    document.getElementById("color-slider-red").value = color.red;
+    document.getElementById("color-slider-red-label").innerText = color.red;
+    document.getElementById("color-slider-green").value = color.green;
+    document.getElementById("color-slider-green-label").innerText = color.green;
+    document.getElementById("color-slider-blue").value = color.blue;
+    document.getElementById("color-slider-blue-label").innerText = color.blue;
+ }
+
 
 // for write hex code
 // output.addEventListener("keyup", function(e){
@@ -138,7 +171,7 @@ function genHexcol({red, green, blue}){
         const hex = color.toString(16);
         return hex.length == 1 ? `0${hex}` : hex;
     }
-    return `#${getTowCode(red)}${getTowCode(green)}${getTowCode(blue)}`.toUpperCase();
+    return `${getTowCode(red)}${getTowCode(green)}${getTowCode(blue)}`.toUpperCase();
 
 }
 
